b083f21e49c362c6267eaf6a38d8815792ff0a14
nikinezakati <nikinezakati@Nikis-MacBook-Pro.local>
2021-04-04 23:28:15 +0430

    fixed user auth

--- members/forms.py
+++ members/forms.py
@@ -1,10 +1,11 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from User.models import user
 from django import forms
 from .models import *
 
+
 class SignupForm(UserCreationForm):
-	class Meta:
-		model = User
-		fields = ['username', 'first_name','last_name','email', 'password1', 'password2']
+    class Meta:
+        model = user
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
